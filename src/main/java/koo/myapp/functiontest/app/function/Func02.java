94b8e0cd8c233f64b705c2ed1def08cfd305aa7d
koichi asano <koo2000@gmail.com>
2024-03-29 14:09:17 +0900

    add second function .

---
+++ src/main/java/koo/myapp/functiontest/app/function/Func02.java
@@ -0,0 +1,47 @@
+package koo.myapp.functiontest.app.function;
+
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.HttpMethod;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.HttpStatus;
+import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.HttpTrigger;
+
+import koo.myapp.functiontest.app.InjectedObj;
+
+/**
+ * Azure Functions with HTTP Trigger.
+ */
+@Component
+public class Func02 {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private InjectedObj obj;
+    /**
+     * This function listens at endpoint "/api/HttpExample". Two ways to invoke it using "curl" command in bash:
+     * 1. curl -d "HTTP Body" {your host}/api/HttpExample
+     * 2. curl "{your host}/api/HttpExample?name=HTTP%20Query"
+     */
+    @FunctionName("HttpExample2")
+    public HttpResponseMessage run(
+            @HttpTrigger(
+                name = "req",
+                methods = {HttpMethod.GET, HttpMethod.POST},
+                authLevel = AuthorizationLevel.ANONYMOUS)
+                HttpRequestMessage<Optional<String>> request,
+            final ExecutionContext context) {
+        logger.info("Func 02 called");
+        return request.createResponseBuilder(HttpStatus.OK).body("Func02 called").build();
+    }
+}
